78741ac6c40b281a6e6939ab928020bdf60c6779
saeede <saeede.p.000@gmail.com>
2025-03-06 18:31:15 +0330

    create dashboard

--- app/panel/[slug]/@services/page.tsx
+++ app/panel/[slug]/@services/page.tsx
@@ -1,20 +1,10 @@
-import Services from '@/app/panel/[slug]/@services/services'
+import Services from "@/app/panel/[slug]/@services/services";
 
-
-export default function (){
-        
-    
-    
-    
-    
-    return (
-    <main >
-    
-    <h2 >
-    
-    services
-    </h2>
-    <Services />
+export default function () {
+  return (
+    <main>
+      <h2>services</h2>
+      <Services />
     </main>
-    )
-    }
+  );
+}
