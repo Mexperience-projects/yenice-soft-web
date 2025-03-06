37287e4d88bc0f0c167343df781765c678f0c9d7
aliroide <aliroide@gmail.com>
2025-03-06 16:12:51 +0330

    update axios

--- lib/axios/noUser.ts
+++ lib/axios/noUser.ts
@@ -2,5 +2,45 @@ import axios from "axios";
 import { SERVER_URL } from "../core";
 
 export const axiosUser = axios.create({
-    baseURL: SERVER_URL,
+    baseURL: SERVER_URL,  
 });
+
+axiosUser.interceptors.request.use(
+    (config) => {
+      const token = localStorage.getItem('token');
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    },
+    (error) => Promise.reject(error)
+  );
+
+
+  
+  axiosUser.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      if (error.response.status === 401) {
+        // Handle token expiration, e.g., refresh the token
+        const refreshToken = localStorage.getItem('refresh');
+        if (refreshToken) {
+          return axios.post('/user/refresh', { refreshToken })
+            .then((res) => {
+              localStorage.setItem('token', res.data.token);
+              error.config.headers.Authorization = `Bearer ${res.data.token}`;
+              return axiosUser(error.config);
+            })
+            .catch((err) => {
+              localStorage.removeItem('token');
+              localStorage.removeItem('refresh');
+              // Redirect to login or handle as needed
+              return Promise.reject(err);
+            });
+        }
+      }
+      return Promise.reject(error);
+    }
+  );
+
+  export default axiosUser;
