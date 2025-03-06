e26d07dc4ed247ff7043c11ad9a5cbf1c6e8325b
algonouir <algo.mahdi.nouri@gmail.com>
2025-03-06 16:04:29 +0330

    delete stores and layout of redux

--- app/guard.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { store } from "@/store/store";
-import { ReactNode } from "react";
-import { Provider } from "react-redux";
-
-export default function Gaurd(props: { children: ReactNode }) {
-    return <Provider store={store}>{props.children}</Provider>;
-}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import "./globals.css";
-import Gaurd from "./guard";
 
 export default function RootLayout({
     children,
@@ -8,9 +7,7 @@ export default function RootLayout({
 }) {
     return (
         <html lang="fa">
-            <Gaurd>
-                <body>{children}</body>
-            </Gaurd>
+            <body>{children}</body>
         </html>
     );
 }
