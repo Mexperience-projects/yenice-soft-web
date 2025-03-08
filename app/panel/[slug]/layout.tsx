858293a3dd3ad10870835b7a565c09f5450efbfd
aliroide <aliroide@gmail.com>
2025-03-08 13:28:28 +0330

    debug build errors

--- app/panel/[slug]/layout.tsx
+++ app/panel/[slug]/layout.tsx
@@ -20,20 +20,21 @@ export default function ({
   items,
   services,
 }: _LayoutType) {
-  // render content for meny
-  const renderContent = async () =>
-    ({
-      dashboard,
-      visit,
-      presonels,
-      items,
-      services,
-    })[(await params).slug];
+  // // render content for meny
+  // const renderContent = async () =>
+  //   ({
+  //     dashboard,
+  //     visit,
+  //     presonels,
+  //     items,
+  //     services,
+  //   })[(await params).slug];
 
   return (
     <div className="flex">
       <Menu />
-      <main className="w-full">{renderContent()}</main>
+      {/* <main className="w-full">{renderContent()}</main> */}
+      <main className="w-full">{}</main>
     </div>
   );
 }
--- cypress/e2e/panel/visit.cy.ts
+++
@@ -1,2 +0,0 @@
-import React from 'react'
-import { Visit } from '@/app/panel/[slug]/@visit/visit'
